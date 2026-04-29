194df3093d8dfce4130174ec4dc37c0bd9b31def
mario eduard stoica <122436182+Marker1516@users.noreply.github.com>
2026-04-29 10:00:57 +0200

    Create utilitl.c

---
+++ utilitl.c
@@ -0,0 +1,10 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void logmsg(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+}
